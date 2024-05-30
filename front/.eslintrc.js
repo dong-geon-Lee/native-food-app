3eebc57a8e523533f5306d8f8d694f7561cff35e
developer <ch9716@naver.com>
2024-05-30 14:45:31 +0900

    학습

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 };
--- front/App.tsx
+++ front/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Ap1p</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Text>이름</Text>
+        <TextInput style={styles.input} />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'black',
+    width: 50,
+    height: 50,
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: 'beige',
+    flexDirection: 'row',
+    alignItems: 'center',
+    // paddingHorizontal: 30,
+    // paddingVertical: 30,
+  },
+});
 
 export default App;
