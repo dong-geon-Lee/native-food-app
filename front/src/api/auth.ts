86be843ce4fa5dc1a1c6ef6800948de4a053def4
developer <ch9716@naver.com>
2024-06-05 19:21:52 +0900

    백엔드 코드 추가 및 프론트 코드 설정.

---
+++ front/src/api/auth.ts
@@ -0,0 +1,56 @@
+import {Category, Profile} from '../types/domain';
+import {getEncryptStorage} from '../utils';
+import axiosInstance from './axios';
+
+export type RequestUser = {
+  email: string;
+  password: string;
+};
+
+export type ResponseToken = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+export type ResponseProfile = Profile & Category;
+
+export const postSignup = async ({
+  email,
+  password,
+}: RequestUser): Promise<void> => {
+  const {data} = await axiosInstance.post('/auth/signup', {
+    email,
+    password,
+  });
+
+  return data;
+};
+
+export const postLogin = async ({
+  email,
+  password,
+}: RequestUser): Promise<ResponseToken> => {
+  const {data} = await axiosInstance.post('/auth/signin', {
+    email,
+    password,
+  });
+
+  return data;
+};
+
+export const getProfile = async (): Promise<ResponseProfile> => {
+  const {data} = await axiosInstance.get('/auth/me');
+  return data;
+};
+
+export const getAccessToken = async () => {
+  const refreshToken = await getEncryptStorage('refreshToken');
+  const {data} = await axiosInstance.get('/auth/refresh', {
+    headers: {Authorization: `Bearer ${refreshToken}`},
+  });
+  return data;
+};
+
+export const logout = async () => {
+  await axiosInstance.post('/auth/logout');
+};
